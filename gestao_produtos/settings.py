b8853cd2fc83bf0b4b531b470b9d9efbe3268e8b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2020-05-17 19:01:13 -0300

    Configurando app para deploy

--- gestao_produtos/settings.py
+++ gestao_produtos/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['wmf-gestao-produtos.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
